65c51de5425553864aa7a2884a6ef4305fa86009
liuqingliang <liuqingliang@meituan.com>
2018-11-30 23:32:17 +0800

    链表环判断

---
+++ src/double_index/LinkedCycle_141.java
@@ -0,0 +1,29 @@
+package double_index;
+
+public class LinkedCycle_141 {
+    public boolean hasCycle(ListNode head) {
+        if (head == null) {
+            return false;
+        }
+        ListNode l1 = head;
+        ListNode l2 = head.next;
+        while (l2 != null && l2.next != null) {
+            if (l1 == l2) {
+                return true;
+            }
+            l1 = l1.next;
+            l2 = l2.next.next;
+        }
+        return false;
+    }
+}
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+        next = null;
+    }
+}
